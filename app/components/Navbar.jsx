2a7c5772ce60a2df252a9f5e4563503c775746f7
enricojunior <zethsuboaragonchemistry@gmail.com>
2024-09-29 23:19:37 +0800

    Commit Tutorials

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,18 @@
+import Image from "next/image"
 import Link from "next/link"
+import Logo from "./dojo-logo.png"
 
 export default function Navbar() {
   return (
     <nav>
-        <h1>Dojo Helpdesk</h1>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
+      <Image
+        src={Logo}
+        alt='Dojo Helpdesk Logo'
+        width={70} 
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
     </nav>
   )
 }
